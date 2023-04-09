43eec3497eacb24fc680b9fe8f6aa559e0c70cd3
Amaniaz <Amaniaz2006@mail.ru>
2023-04-09 21:38:20 +0300

    'calcapp'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import React , {useState} from 'react';
 
 function App() {
+  let [tocount , setCount] = useState('')
+  
+  let nums = ['1' , '2' , '3' , '4' , '5' , '6' , '7' , '8' , '9' , '0']
+
+  let opers = ['+' , '-' , '*' , '/']
+
+  let htmlopers = opers.map((item) => (
+    <button onClick={() => addoper(item)}>{item}</button>
+  ))
+
+  let htmlnums = nums.map((item) => (
+    <button onClick={() => addnum(item)}>{item}</button>
+  ))
+
+  function addnum(num) {
+    setCount(tocount += num)
+  }
+
+  function addoper(oper) {
+      if(tocount[tocount.length - 1] != '+' && tocount[tocount.length - 1] != '-' && tocount[tocount.length - 1] != '/' && tocount[tocount.length - 1] != '*'){
+          setCount(tocount += oper)
+      }
+  }
+
+  function tores() {
+    if(tocount[tocount.length - 1] != '+' && tocount[tocount.length - 1] != '-' && tocount[tocount.length - 1] != '/' && tocount[tocount.length - 1] != '*'){
+      let result = eval(tocount)
+      setCount(result)
+    }
+  }
+
+  function toclear() {
+    setCount('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='calculator'>
+        <div className='result'>{tocount}</div>
+        <div className='calcgrid'>
+          {htmlopers}
+          {htmlnums}
+          <button onClick={tores}>=</button>
+          <button onClick={toclear}>C</button>
+        </div>
+      </div>
+    </>
   );
 }
 
